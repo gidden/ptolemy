6e2e3d3f7966aa923cd593dd64079adf6c4eaefc
Matthew Gidden <matthew.gidden@gmail.com>
2017-04-26 08:40:26 +0200

    initial commit

---
+++ doc/foo.rst
@@ -0,0 +1,5 @@
+Documentation for the Code
+**************************
+
+.. automodule:: ptolemy.foo
+   :members:
---
+++ tests/test_foo.py
@@ -0,0 +1,6 @@
+from nose.tools import assert_equal
+
+from ptolemy import foo
+
+def test_bar():
+    assert_equal(foo.bar(), 42)
